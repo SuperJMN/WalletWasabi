808e1d5b420799fe822ccbcb9606c169c1e04da2
José Manuel Nieto <superjmn@outlook.com>
2023-01-29 17:59:27 +0100

    Improvements

--- WalletWasabi.Fluent/Controls/ReceiveAddressControl.axaml.cs
+++ WalletWasabi.Fluent/Controls/ReceiveAddressControl.axaml.cs
@@ -1,12 +1,18 @@
 using System.Collections.Generic;
 using Avalonia;
 using Avalonia.Controls.Primitives;
-using WalletWasabi.Fluent.ViewModels.Wallets.Receive;
 
 namespace WalletWasabi.Fluent.Controls;
+
 public class ReceiveAddressControl : TemplatedControl
 {
-	public static readonly StyledProperty<object> ExtraContentProperty = AvaloniaProperty.Register<ReceiveAddressControl, object>("ExtraContent");
+	public static readonly StyledProperty<object> ExtraContentProperty = AvaloniaProperty.Register<ReceiveAddressControl, object>(nameof(ExtraContent));
+
+	public static readonly StyledProperty<string> AddressProperty = AvaloniaProperty.Register<ReceiveAddressControl, string>(nameof(Address));
+
+	public static readonly StyledProperty<IEnumerable<string>> LabelsProperty = AvaloniaProperty.Register<ReceiveAddressControl, IEnumerable<string>>(nameof(Labels));
+
+	public static readonly StyledProperty<bool[,]> QrCodeProperty = AvaloniaProperty.Register<ReceiveAddressControl, bool[,]>(nameof(QrCode));
 
 	public object ExtraContent
 	{
@@ -14,31 +20,17 @@ public class ReceiveAddressControl : TemplatedControl
 		set => SetValue(ExtraContentProperty, value);
 	}
 
-	public static readonly StyledProperty<IReceiveAddressViewModel> ReceiveAddressProperty = AvaloniaProperty.Register<ReceiveAddressControl, IReceiveAddressViewModel>("ReceiveAddress");
-
-	public IReceiveAddressViewModel ReceiveAddress
-	{
-		get => GetValue(ReceiveAddressProperty);
-		set => SetValue(ReceiveAddressProperty, value);
-	}
-
-	public static readonly StyledProperty<string> AddressProperty = AvaloniaProperty.Register<ReceiveAddressControl, string>("Address");
-
 	public string Address
 	{
 		get => GetValue(AddressProperty);
 		set => SetValue(AddressProperty, value);
 	}
 
-	public static readonly StyledProperty<IEnumerable<string>> LabelsProperty = AvaloniaProperty.Register<ReceiveAddressControl, IEnumerable<string>>("Labels");
-
 	public IEnumerable<string> Labels
 	{
 		get => GetValue(LabelsProperty);
 		set => SetValue(LabelsProperty, value);
 	}
-
-	public static readonly StyledProperty<bool[,]> QrCodeProperty = AvaloniaProperty.Register<ReceiveAddressControl, bool[,]>("QrCode");
 
 	public bool[,] QrCode
 	{
--- WalletWasabi.Fluent/ViewModels/Wallets/Receive/ReceiveAddressViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Wallets/Receive/ReceiveAddressViewModel.cs
@@ -5,7 +5,7 @@ using WalletWasabi.Bridge;
 
 namespace WalletWasabi.Fluent.ViewModels.Wallets.Receive;
 
-public class ReceiveAddressViewModel : ReactiveObject, IReceiveAddressViewModel
+public class ReceiveAddressViewModel : ReactiveObject
 {
 	private readonly ObservableAsPropertyHelper<bool[,]> _qrCode;
 
