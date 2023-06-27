05c4036b7d23af7d47725d21b133f8ed96e6253c
José Manuel Nieto <superjmn@outlook.com>
2023-06-27 15:29:17 +0200

    Improvements

--- WalletWasabi.Fluent/MyFormatter.cs
+++ WalletWasabi.Fluent/MyFormatter.cs
@@ -6,5 +6,5 @@ public class MyFormatter
 {
 	public static FuncValueConverter<decimal?, string> Instance { get; } = new(d => d?.ToString("0.#### ####") ?? "");
 	public static string BtcFormat { get; } = "0.#### ####";
-	public static string UsdFormat { get; } = "0.##";
+	public static string UsdFormat { get; } = "≈ 0.##";
 }
