71ced5ce57fdebec512e8402d3e10a3e16156727
José Manuel Nieto <superjmn@outlook.com>
2023-06-30 16:35:22 +0200

    Improvements

--- WalletWasabi.Fluent/Controls/DecimalTextEntry.axaml.cs
+++ WalletWasabi.Fluent/Controls/DecimalTextEntry.axaml.cs
@@ -4,7 +4,9 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
 using Avalonia.Data;
+using Avalonia.Layout;
 using Avalonia.Markup.Xaml.Templates;
+using Avalonia.Media;
 using ReactiveUI;
 
 namespace WalletWasabi.Fluent.Controls;
@@ -22,6 +24,36 @@ public class DecimalTextEntry : TemplatedControl
 	public static readonly StyledProperty<string?> FormatProperty = AvaloniaProperty.Register<DecimalTextEntry, string?>(nameof(Format));
 
 	public static readonly StyledProperty<TextBox?> TextBoxProperty = AvaloniaProperty.Register<DecimalTextEntry, TextBox?>(nameof(TextBox));
+
+	public static readonly StyledProperty<TextAlignment> TextAlignmentProperty = AvaloniaProperty.Register<DecimalTextEntry, TextAlignment>(nameof(TextAlignment));
+
+	/// <summary>
+	/// Defines the <see cref="HorizontalAlignment"/> property.
+	/// </summary>
+	public static readonly StyledProperty<HorizontalAlignment> HorizontalContentAlignmentProperty = ContentControl.HorizontalContentAlignmentProperty.AddOwner<DecimalTextEntry>();
+
+	/// <summary>
+	/// Defines the <see cref="VerticalAlignment"/> property.
+	/// </summary>
+	public static readonly StyledProperty<VerticalAlignment> VerticalContentAlignmentProperty = ContentControl.VerticalContentAlignmentProperty.AddOwner<DecimalTextEntry>();
+
+	/// <summary>
+	/// Gets or sets the horizontal alignment of the content within the control.
+	/// </summary>
+	public HorizontalAlignment HorizontalContentAlignment
+	{
+		get { return GetValue(HorizontalContentAlignmentProperty); }
+		set { SetValue(HorizontalContentAlignmentProperty, value); }
+	}
+
+	/// <summary>
+	/// Gets or sets the vertical alignment of the content within the control.
+	/// </summary>
+	public VerticalAlignment VerticalContentAlignment
+	{
+		get { return GetValue(VerticalContentAlignmentProperty); }
+		set { SetValue(VerticalContentAlignmentProperty, value); }
+	}
 
 	private string? _formattedValue;
 
@@ -43,6 +75,12 @@ public class DecimalTextEntry : TemplatedControl
 		this.WhenAnyValue(x => x.Value)
 			.Do(v => Text = v?.ToString(CultureInfo.CurrentCulture) ?? "")
 			.Subscribe();
+	}
+
+	public TextAlignment TextAlignment
+	{
+		get => GetValue(TextAlignmentProperty);
+		set => SetValue(TextAlignmentProperty, value);
 	}
 
 	public string? Format
