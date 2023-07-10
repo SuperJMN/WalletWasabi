35807072680a48a797832987d7ea24c30e32f8e4
José Manuel Nieto <superjmn@outlook.com>
2023-07-10 11:48:20 +0200

    Money format

--- WalletWasabi.Fluent/Helpers/MoneyFormat.cs
+++ WalletWasabi.Fluent/Helpers/MoneyFormat.cs
@@ -3,5 +3,5 @@ namespace WalletWasabi.Fluent.Helpers;
 public static class MoneyFormat
 {
 	public static string Btc => "0.#### ####";
-	public static string Usd => "C";
+	public static string Usd => "0.00";
 }
