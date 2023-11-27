3893e1491b26bde0de9298124c4fc382d240f383
José Manuel Nieto <superjmn@outlook.com>
2023-11-27 21:11:27 +0100

    Apply XAML componentalization

---
+++ WalletWasabi.Fluent/Views/Wallets/Buy/CurrentConversationView.axaml.cs
@@ -0,0 +1,16 @@
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace WalletWasabi.Fluent.Views.Wallets.Buy;
+public partial class CurrentConversationView : UserControl
+{
+	public CurrentConversationView()
+	{
+		InitializeComponent();
+	}
+
+	private void InitializeComponent()
+	{
+		AvaloniaXamlLoader.Load(this);
+	}
+}
